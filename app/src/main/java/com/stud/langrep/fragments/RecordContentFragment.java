1769701e3671dad6aacf8b5aace3a339c21088c5
lubuntum <lubuntum@gmail.com>
2023-04-29 13:54:13 +0700

    Добавил опционное меню по нажатию плавающей кнопки

--- app/src/main/java/com/stud/langrep/fragments/RecordContentFragment.java
+++ app/src/main/java/com/stud/langrep/fragments/RecordContentFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.stud.langrep.R;
 import com.stud.langrep.adapters.WordsAdapter;
 import com.stud.langrep.database.entity.Record;
 import com.stud.langrep.database.entity.Word;
@@ -22,6 +25,7 @@ import java.util.List;
 public class RecordContentFragment extends Fragment {
     RecordContentFragmentBinding binding;
     public static final String RECORD_KEY = "record";
+    public boolean isOptionsVisible = false;
     private Record record;
     private WordsAdapter adapter;
     RecordViewModel viewModel;
@@ -49,10 +53,13 @@ public class RecordContentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         wordListInit();
         addWordInit();
+        startPracticeInit();
+        showOptionsInit();
     }
     public void wordListInit(){
         if(record.getWords() != null && record.getWords().size() > 0){
             adapter = new WordsAdapter(getContext(), record.getWords());
+            binding.wordsList.setAdapter(adapter);
             return;
         }
         Observer<List<Word>> wordListObserver = (words)->{
@@ -70,10 +77,37 @@ public class RecordContentFragment extends Fragment {
             Word word = new Word("","",record.getId());
             if(adapter == null) {
                 adapter = new WordsAdapter(getContext(), new LinkedList<>());
-                record.setWords(adapter.getWordList());
+                binding.wordsList.setAdapter(adapter);
+
+                //record.setWords(adapter.getWordList());
             }
             adapter.addWord(word);
         });
+    }
+    public void startPracticeInit(){
+        binding.startPractice.setOnClickListener((view)->{
+            Toast.makeText(getContext(), "Practice!", Toast.LENGTH_SHORT).show();
+        });
+    }
+    public void showOptionsInit(){
+        binding.options.setOnClickListener((view -> {
+            changeOptionsState();
+        }));
+    }
+    public void changeOptionsState(){
+        if(isOptionsVisible){
+            binding.addWord.setVisibility(View.INVISIBLE);
+            binding.startPractice.setVisibility(View.INVISIBLE);
+            binding.options.setImageDrawable(
+                    AppCompatResources.getDrawable(getContext(),R.drawable.ic_arrow_up));
+        }
+        else {
+            binding.addWord.setVisibility(View.VISIBLE);
+            binding.startPractice.setVisibility(View.VISIBLE);
+            binding.options.setImageDrawable(
+                    AppCompatResources.getDrawable(getContext(),R.drawable.ic_arrow_down));
+        }
+        isOptionsVisible = !isOptionsVisible;
     }
 
     @Override
